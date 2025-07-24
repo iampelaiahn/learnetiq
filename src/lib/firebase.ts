da16147f1deb6a806ac2e726290b9b2898dcf728
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-07-24 16:19:05 +0000

    its still not working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDWzKLK195Vo2PiYh6Nd63hLY94TmmMt0E",
+  authDomain: "learnetiq-ncqpq.firebaseapp.com",
+  projectId: "learnetiq-ncqpq",
+  storageBucket: "learnetiq-ncqpq.firebasestorage.app",
+  messagingSenderId: "61995232955",
+  appId: "1:61995232955:web:69a432f8a141cb3dd4439e"
 };
 
 // Initialize Firebase
