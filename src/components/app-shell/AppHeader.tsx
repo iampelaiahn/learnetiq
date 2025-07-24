69121a374a7f017ff01cfcdbaeb9647d9daa728e
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-07-24 17:37:15 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/app-shell/AppHeader.tsx
+++ src/components/app-shell/AppHeader.tsx
@@ -1,21 +1,31 @@
+'use client';
 import { Input } from '../ui/input';
-import { SidebarTrigger } from '../ui/sidebar';
+import { SidebarTrigger, useSidebar } from '../ui/sidebar';
 import { Search } from 'lucide-react';
 import { UserNav } from './UserNav';
 import { Logo } from '../Logo';
-import Link from 'next/link';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { ThemeToggle } from '../ThemeToggle';
+import { cn } from '@/lib/utils';
 
 export function AppHeader() {
+  const { state: sidebarState } = useSidebar();
+  const isMobile = useIsMobile();
+
+  const isSidebarCollapsed = sidebarState === 'collapsed';
+
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-       <div className="hidden md:block">
-         <SidebarTrigger />
-       </div>
-       <div className="md:hidden">
-         <Logo />
-       </div>
+      <div
+        className={cn('flex items-center gap-2', {
+          'md:hidden': !isSidebarCollapsed,
+        })}
+      >
+        <Logo />
+      </div>
+      <div className="hidden md:block">
+        <SidebarTrigger />
+      </div>
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
         <form className="ml-auto flex-1 sm:flex-initial">
           <div className="relative">
