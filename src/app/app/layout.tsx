3759d3546826f295c45c6b5c9c25dc951a5ff537
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-08-25 13:47:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <main class="min-h

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,19 +1,39 @@
+
+'use client';
+
 import type { PropsWithChildren } from 'react';
 import { AppSidebar } from '@/components/app-shell/AppSidebar';
-import { SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarProvider, useSidebar } from '@/components/ui/sidebar';
 import { AppHeader } from '@/components/app-shell/AppHeader';
+import { useIsMobile } from '@/hooks/use-mobile';
+import { cn } from '@/lib/utils';
+
+function AppContent({ children }: PropsWithChildren) {
+  const { state } = useSidebar();
+  const isMobile = useIsMobile();
+  
+  const marginLeft = isMobile ? '0' : state === 'expanded' ? '16rem' : '4rem';
+
+  return (
+    <div 
+      className="flex flex-1 flex-col transition-[margin-left] duration-300 ease-in-out"
+      style={{ marginLeft }}
+    >
+      <AppHeader />
+      <main className="min-h-[calc(100vh-4rem)] p-4 md:p-8">
+        {children}
+      </main>
+    </div>
+  );
+}
+
 
 export default function AppLayout({ children }: PropsWithChildren) {
   return (
     <SidebarProvider>
       <div className="flex">
         <AppSidebar />
-        <div className="flex flex-1 flex-col md:ml-16">
-          <AppHeader />
-          <main className="min-h-[calc(100vh-4rem)] p-4 md:p-8">
-            {children}
-          </main>
-        </div>
+        <AppContent>{children}</AppContent>
       </div>
     </SidebarProvider>
   );
