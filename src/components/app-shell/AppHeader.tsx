373ffcb1fa52a7ac6adb292a39e461131445d6c9
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-08-25 14:18:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AppHeader>
    
    with

--- src/components/app-shell/AppHeader.tsx
+++ src/components/app-shell/AppHeader.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 import { SidebarTrigger } from '../ui/sidebar';
 import { UserNav } from './UserNav';
@@ -6,11 +7,8 @@ import { ThemeToggle } from '../ThemeToggle';
 export function AppHeader() {
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-      <div className="flex items-center gap-2">
-        <SidebarTrigger />
-      </div>
-      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <div className="ml-auto flex-1 sm:flex-initial" />
+      <SidebarTrigger />
+      <div className="flex flex-1 items-center justify-end gap-4">
         <ThemeToggle />
         <UserNav />
       </div>
